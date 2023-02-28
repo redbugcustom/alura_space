3fa44d133a0b201e810a7dbd0a390e8bac27edf7
redbugcustom <bugcustom@hotmail.com>
2023-02-28 15:28:03 -0300

    atualizando

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#class Fotografia(models.Model):
+ #   nome = models.CharField(null=False, blank=False)
+  #  legenda = models.CharField(null=false, blank=false)
+   # descricao = models.TextField(null=False, blank=False)
+    #foto = models.CharField(null=false, blank=False)
+
+    #def __str__(self) -> str:
+     #   return f"super [nome = {self.nome}]"
+    
