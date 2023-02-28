dfbd960761777db264e5c1b468bc4717691220f5
redbugcustom <bugcustom@hotmail.com>
2023-02-28 17:35:31 -0300

    mais atualizacao

--- galeria/views.py
+++ galeria/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-          
+from galeria.models import Fotografia
 
+def index(request):
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def index (request):
-   
-   dados = {
-
-      1: {"Nome": "Nebulosa de Carina", "Legenda": "webbtelescope.org / NASA / James Webb"},
-      2: {"Nome": "Galaxia NGC 1079", "Legenda": "nasa.org / NASA / Hubble"}
-   
-   }
-
-   return render(request, 'galeria/index.html', {"cards": dados})
-
-def imagem (request):
-   return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
